c008019541cc4002e9bf003e31b314f3fe574f09
ianrackson <nasirus@protonmail.com>
2021-05-16 19:40:09 -0400

    remove unnesseary script

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -19,16 +19,6 @@ const Markdown = (props) => {
                         __html: props.sanitizedHtml,
                     }}
                 ></div>
-                <script
-                    src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/prism.min.js"
-                    integrity="sha512-YBk7HhgDZvBxmtOfUdvX0z8IH2d10Hp3aEygaMNhtF8fSOvBZ16D/1bXZTJV6ndk/L/DlXxYStP8jrF77v2MIg=="
-                    crossOrigin="anonymous"
-                ></script>
-                <script
-                    src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js"
-                    integrity="sha512-zc7WDnCM3aom2EziyDIRAtQg1mVXLdILE09Bo+aE1xk0AM2c2cVLfSW9NrxE5tKTX44WBY0Z2HClZ05ur9vB6A=="
-                    crossOrigin="anonymous"
-                ></script>
             </div>
         </>
     );
