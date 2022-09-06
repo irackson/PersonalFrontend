a959dd558ca099f4ad6f29f530655aab48129829
irackson <nasirus@protonmail.com>
2022-09-05 21:36:51 -0400

    swap metrics

--- src/pages/Metrics.jsx
+++ src/pages/Metrics.jsx
@@ -1,55 +1,52 @@
-import 'styles/vendors/GitCalStyle.css';
+//import GitHubCalendar from 'github-calendar';
+//import { useEffect } from 'react';
+//import 'styles/vendors/GitCalStyle.css';
 import useWindowDimensions from 'utils/window-size';
-import GitHubCalendar from 'github-calendar';
-import { useEffect } from 'react';
 
 const Metrics = () => {
-    useEffect(() => {
-        document.title = 'Ian Rackson | Metrics';
-    }, []);
+  useEffect(() => {
+    document.title = 'Ian Rackson | Metrics';
+  }, []);
 
-    const { height, width } = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
 
-    const getCal = () => {
-        GitHubCalendar('.calendar', 'irackson', {
-            // // summary_text:
-            //     'Summary of my Github pull requests, issues opened, and commits',
-            responsive: true,
-            tooltips: true,
-        });
-    };
+  /* const getCal = () => {
+    GitHubCalendar('.calendar', 'irackson', {
+      // // summary_text:
+      //     'Summary of my Github pull requests, issues opened, and commits',
+      responsive: true,
+      tooltips: true,
+    });
+  };
 
-    useEffect(() => {
-        getCal();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    getCal();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); */
 
-    return (
-        <>
-            <section>
-                <div className="calendar"></div>
-            </section>
-            {/* WAKATIME COLOR: #067526 */}
-            <section className="wakatime">
-                {/* <figure> */}
-                <figure style={{ display: 'none' }}>
-                    <embed
-                        width={Math.floor(width * (80 / 100))}
-                        height={Math.floor(height * (60 / 100))}
-                        src="https://wakatime.com/share/@irackson/be93eb6f-c4dc-48b7-9c63-b67ddabdda73.svg"
-                        title="coding activity graph"
-                    />
-                </figure>
-                <figure>
-                    <embed
-                        width={Math.floor(width * (80 / 100))}
-                        height={Math.floor(height * (60 / 100))}
-                        src="https://wakatime.com/share/@irackson/aaecfd27-6cb3-4a56-9490-b8e0ad12c335.svg"
-                        title="language (time spent)"
-                    />
-                </figure>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section>{/* <div className="calendar"></div> */}</section>
+      {/* WAKATIME COLOR: #067526 */}
+      <section className="wakatime">
+        <figure /* style={{ display: 'none' }} */>
+          <embed
+            width={Math.floor(width * (80 / 100))}
+            height={Math.floor(height * (60 / 100))}
+            src="https://wakatime.com/share/@irackson/be93eb6f-c4dc-48b7-9c63-b67ddabdda73.svg"
+            title="coding activity graph"
+          />
+        </figure>
+        <figure>
+          <embed
+            width={Math.floor(width * (80 / 100))}
+            height={Math.floor(height * (60 / 100))}
+            src="https://wakatime.com/share/@irackson/aaecfd27-6cb3-4a56-9490-b8e0ad12c335.svg"
+            title="language (time spent)"
+          />
+        </figure>
+      </section>
+    </>
+  );
 };
 export default Metrics;
